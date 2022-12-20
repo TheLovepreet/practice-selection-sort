e076c88b80f5c537d3128e9be3e2205105eff2ac
TheLove <lovepreetsinghcad@gmail.com>
2022-12-20 17:55:55 -0500

    selection sort in place done

--- selection-sort.js
+++ selection-sort.js
@@ -18,41 +18,48 @@ function selectionSort(arr) {
   newArr.splice(newArr.indexOf(min),1);
 }
 return sorted;
-  // Create an array to store the sorted values
-
-  // While the array is not empty...
-
-    // Do not move this console.log
-
-    // Find the index of the minimum value in the unsorted half
-
-    // Save and remove the value at the min index
-
-    // Add the min value to the end of the sorted array
-
+  /* Create an array to store the sorted values
+   While the array is not empty...
+   Do not move this console.log
+     Find the index of the minimum value in the unsorted half
+     Save and remove the value at the min index
+     Add the min value to the end of the sorted array
+*/
 }
 
 
 
 function selectionSortInPlace(arr) {
+  for(let divider = 0; divider < arr.length; divider++){
+    console.log(arr.join(','));
+    let min = arr[divider];
+    let minIndex = 0;
+    for(let j = divider + 1; j < arr.length; j++){
+      if(arr[j] <  min){
+        min = arr[j];
+        minIndex = j;
+      }
+    }
+    let Index = minIndex;
+    while(Index > divider){
+      arr[Index] = arr[Index-1];
+      Index--;
+    } 
+    arr[divider] = min;
+    }
+  //[2,4,6,8,1,3,5,7,9]
+
+
+
 
   // Set a pointer at zero diving the array into sorted and unsorted halves
-
   // Repeat while the unsorted half is not empty:
-
     // Do not move this console.log
-    console.log(arr.join(","));
-
     // Find the index of the minimum value in the unsorted half
-
     // Save the min value
-
     // Shift every unsorted value to the left of the min value to the right by 1
-
     // Put the min value at the divider
-
     // Increment the divider and repeat
-
 }
 
 
--- test/sort-specs.js
+++ test/sort-specs.js
@@ -23,7 +23,7 @@ describe ('Basic Sorting Algorithms', function () {
     chai.spy.restore(console, 'log');
   });
 
-
+/*
   it('performs a out-of-place selection sort', function () {
 
     let newArr = selectionSort(arr);
@@ -41,7 +41,7 @@ describe ('Basic Sorting Algorithms', function () {
     expect(newArr).to.deep.equal([1,2,3,4,5,6,7,8,9]);
     expect(arr).to.deep.equal([2,4,6,8,1,3,5,7,9]);
 
-  });
+  });*/
 
   it('performs an in-place selection sort', function () {
 
